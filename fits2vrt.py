7e580e2a1ad2749199d940104284979f992c9a03
Chiara Marmo <chiara.marmo@u-psud.fr>
2016-01-20 14:25:52 +0100

    Adding metadata to Virtual Header.

--- fits2vrt.py
+++ fits2vrt.py
@@ -1,7 +1,7 @@
 """
 This file is part of fits2vrt
-A FITS to GDAL Virtual Header tool
-09/01/2016
+A FITS to GDAL Virtual Header conversion tool
+20/01/2016
 Author : Chiara Marmo (chiara.marmo@u-psud.fr)
 Copyright : CNRS, Universite Paris-Sud
 """
@@ -48,8 +48,35 @@ class fitskeys(object):
         if (fbittype == 8):
             gbittype = gdal.GDT_Byte
         elif (fbittype == 16):
-            gbittype = gdal.GDT_Int16
+            if (BZERO <= 0):
+              gbittype = gdal.GDT_Int16
+            elif (BZERO > 0):
+              gbittype = gdal.GDT_UInt16
+        elif (fbittype == 32):
+            if (BZERO <= 0):
+              gbittype = gdal.GDT_Int32
+            elif (BZERO > 0):
+              gbittype = gdal.GDT_UInt32
+        elif (fbittype == -32):
+            gbittype = gdal.GDT_Float32
+        elif (fbittype == -64):
+            gbittype = gdal.GDT_Float64
+        else:
+            print "Bit Type not supported"
+            print fbittype
+
         dst_ds = driver.Create( vrtname + '.vrt', dimx, dimy, dimz, gbittype )
+
+        # Setting all non mandatory FITS keywords as metadata
+        metadata = {}
+        header = self.__header
+        nometadata = ['SIMPLE','EXTEND','BITPIX','BZERO','BSCALE','COMMENT','NAXIS']
+        for i in range(1, header['NAXIS']+1):
+            nometadata.append('NAXIS'+str(i))
+        for key in header.keys():
+            if key not in nometadata:
+                metadata[key] = str(header[key])
+        dst_ds.SetMetadata( metadata )
 
         # Top Left pixel is bottom left in FITS
         #topleftx = 
@@ -58,6 +85,7 @@ class fitskeys(object):
         #srs = osr.SpatialReference()
         #srs.SetWellKnownGeogCS( '' )
         #dst_ds.SetProjection( srs.ExportToWkt() )
-        # we're done, close properly the dataset
+
+        # Close properly the dataset
         dst_ds = None
 
